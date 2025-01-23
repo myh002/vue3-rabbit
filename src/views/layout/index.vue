5049240080e952c7548b48c3168d9104a29e7bf5
myh2002 <13709216+myh2002@user.noreply.gitee.com>
2025-01-23 17:16:22 +0800

    实现懒加载

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,4 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  layout
-  <router-view></router-view>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
